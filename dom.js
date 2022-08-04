b2d25d98db09de0b98d025fc3fe52dcab9b595e8
George2811 <sanchez28112001@gmail.com>
2022-08-03 23:21:33 -0500

    fixed responsive design

--- dom.js
+++ dom.js
@@ -1,35 +1,9 @@
-console.log("Hola a todos");
-console.log(document.querySelectorAll(".large"));
-console.log(document.getElementById("result"));
-
-//console.log(document.documentElement.getAttribute("lang"));
-//console.log(document.querySelector("input").getAttribute("class"));
-//console.log(document.querySelectorAll("input")[2].getAttribute("class"));
-
 var num1, num2, result;
 var expresion = "", secExpresion = "";
 function multi(num1,num2){return num1*num2;}
 function divi(num1,num2){return num1/num2;}
 function sum(num1,num2){return num1+num2;}
 function rest(num1,num2){return num1-num2;}
-
-/*num1 =Number (document.querySelectorAll("input")[3].getAttribute("value"));
-num2 = Number(document.querySelectorAll("input")[5].getAttribute("value"));
-console.log(multi(num1,num2));*/
-
-
-/*const $button= document.querySelectorAll("input")[3];
-function comprobacion(){
-    num1 = Number($button.getAttribute("value"));
-    num2 = 15;
-    console.log(sum(num1,num2));
-    console.log(event.type);
-    console.log(event.target);
-}
-$button.addEventListener("click",comprobacion);
-*/
-
-//document.querySelector("input").setAttribute("name","1st input");
 
 /*----------------------------*/
 const $button = document.querySelectorAll("input");
@@ -87,20 +61,3 @@ $button.forEach(input =>{
     input.addEventListener("click", numbersEvents);
 });
 
-
-
-/*---------insert button---------*/
-/*const $elemenInput = document.createElement("input");
-$elemenInput.setAttribute("type","button");
-$elemenInput.setAttribute("name","");
-$elemenInput.setAttribute("value","10");
-
-const $fragment = document.createDocumentFragment();
-$fragment.appendChild($elemenInput);
-const $datos = document.querySelector(".data");
-$datos.appendChild($elemenInput);*/
-/*
-innerHTML    --> inyecta codigo HTML
-textContent  --> inyecta SOLO texto
-outerHTML    --> remplaza la etiqueta por un nuevo codigo HTML
-*/
